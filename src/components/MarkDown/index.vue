b8ae07e0e4764826d8a67bff90fd4ba06b8cb9d9
chenxin <chenxin@funtsui.com>
2019-01-23 15:44:31 +0700

    添加灯箱

--- src/components/MarkDown/index.vue
+++ src/components/MarkDown/index.vue
@@ -4,9 +4,17 @@
 
 <script>
 import marked from 'marked'
+import baguetteBox from 'baguettebox.js'
 import hljs from '@/assets/lib/highlight'
 
 const renderer = new marked.Renderer()
+
+renderer.image = function(href, title, text) {
+  let clazz = `img-box ${href.endsWith('#full') ? 'full-box' : ''}`
+  return `<span class="${clazz}"><a href="${href}" data-caption="${text}">
+    <img src="${href}" alt="${text}" /></a>${text ? `<span>◭ ${text}</span>` : ''}</span>`
+}
+
 marked.setOptions({
   renderer,
   highlight: code => hljs.highlightAuto(code).value
@@ -40,12 +48,16 @@ export default {
   methods: {
     marked() {
       this.html = marked(this.content)
-      // 并不是每个 marked 都需要高亮处理
-      if (!this.target) return
+
       this.$nextTick(() => {
-        hljs.initLineNumbersOnLoad({
-          target: this.target
-        })
+        baguetteBox.run('.markdown')
+
+        // 并不是每个 marked 都需要高亮处理
+        if (this.target) {
+          hljs.initLineNumbersOnLoad({
+            target: this.target
+          })
+        }
       })
     }
   }
