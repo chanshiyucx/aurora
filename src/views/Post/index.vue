d265ebde9ee1df358eea64e49629b5bbee2a2708
chenxin <chenxin@funtsui.com>
2019-01-24 16:45:38 +0700

    post cover

--- src/views/Post/index.vue
+++ src/views/Post/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="post">
-    <article class="card"><MarkDown :content="post.body" target="#post" /></article>
+    <article v-if="post" class="card">
+      <div class="post-header">
+        <img alt :src="post.cover.src" />
+        <div>
+          <div class="title">
+            <h1>{{ post.title }}</h1>
+            <span>{{ post.cover.title }}</span>
+          </div>
+          <div class="meta">
+            <span> <i class="icon icon-calendar"></i> {{ post.created_at }} </span>
+            <span> <i class="icon icon-fire"></i> 热度{{ post.times || 1 }}℃ </span>
+            <span> <i class="icon icon-bookmark-empty"></i> {{ post.milestone.title }} </span>
+            <span>
+              <i class="icon icon-tag"></i>
+              <span v-for="label in post.labels" :key="label.id">{{ label.name }}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="post-body"><MarkDown :content="post.body" target="#post" /></div>
+    </article>
   </div>
 </template>
 
@@ -15,7 +35,7 @@ export default {
   data() {
     return {
       number: '',
-      post: {}
+      post: ''
     }
   },
   created() {
