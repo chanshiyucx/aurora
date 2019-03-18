ea0d445be0fd3b679303fc76947a2b0aafbd1842
chanshiyu <chanshiyu@tg10010.com>
2019-03-18 16:57:25 +0700

    fix visitor

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
     // 统计访客来源
     const referrer = getLocation(document.referrer)
     const hostname = referrer.hostname || '直接访问'
-    if (!hostname.include('chanshiyu.com')) {
+    if (!hostname.includes('chanshiyu.com')) {
       this.$store.dispatch('visitorStatistics', hostname || '直接访问')
     }
   },
