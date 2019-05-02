099e633036b1401129904265568ce3bb997ad077
chanshiyu <chanshiyu@tg10010.com>
2019-05-02 09:15:06 +0700

    feat: 顶部导航自由展示

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -4,7 +4,7 @@
       <RouterLink class="title" to="/">{{ $config.title }}</RouterLink>
       <span class="subtitle">{{ $config.subtitle }}</span>
       <ul>
-        <li v-for="menu in menus" :key="menu.name">
+        <li v-for="menu in displayMenus" :key="menu.name">
           <RouterLink :class="['icon', `icon-${menu.icon}`]" :to="menu.name">{{ menu.title }}</RouterLink>
         </li>
       </ul>
@@ -16,20 +16,25 @@
 export default {
   name: 'Header',
   data() {
+    const { archiveOpts, categoryOpts, tagOpts, moodOpts, bookOpts, friendOpts, aboutOpts } = this.$config
     return {
       menus: [
-        { name: '/', icon: 'shop', title: '首页' },
-        { name: '/archive', icon: 'inbox', title: '归档' },
-        { name: '/category', icon: 'bookmark-empty', title: '分类' },
-        { name: '/tag', icon: 'tag', title: '标签' },
-        { name: '/mood', icon: 'comment', title: '心情' },
-        { name: '/book', icon: 'pencil', title: '书单' },
-        { name: '/friend', icon: 'heart', title: '友链' },
-        { name: '/about', icon: 'universal-access', title: '关于' }
+        { name: '/', display: true, icon: 'shop', title: '首页' },
+        { name: '/archive', display: archiveOpts.display, icon: 'inbox', title: '归档' },
+        { name: '/category', display: categoryOpts.display, icon: 'bookmark-empty', title: '分类' },
+        { name: '/tag', display: tagOpts.display, icon: 'tag', title: '标签' },
+        { name: '/mood', display: moodOpts.display, icon: 'comment', title: '心情' },
+        { name: '/book', display: bookOpts.display, icon: 'pencil', title: '书单' },
+        { name: '/friend', display: friendOpts.display, icon: 'heart', title: '友链' },
+        { name: '/about', display: aboutOpts.display, icon: 'universal-access', title: '关于' }
       ]
     }
   },
-  methods: {}
+  computed: {
+    displayMenus() {
+      return this.menus.filter(o => o.display)
+    }
+  }
 }
 </script>
 
--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
+/**
+ * Aurora 主题配置文件
+ * 食用指南：https://github.com/chanshiyucx/blog/issues/41
+ * 联系作者：Blog https://chanshiyu.com     QQ 1124590931
+ */
+
 export default {
   /**
    * ========================================
@@ -13,12 +19,23 @@ export default {
 
   /**
    * Github Issues 配置【文章、说说、书单、友链】
-   * Github Issues api: https://developer.github.com/v3/issues/
-   **/
+   */
   username: 'chanshiyucx', // github 用户名
-  repository: 'blog', // 仓库地址
+  repository: 'blog', // 文章仓库地址
   // token 从中间任意位置拆开成两部分，避免 github 代码检测失效
   token: ['0ad1a0539c5b96fd18fa', 'aaafba9c7d1362a5746c'],
+
+  /**
+   * Gitalk 配置【评论功能】，详细文档参见：https://github.com/gitalk/gitalk
+   */
+  gitalk: {
+    clientID: '864b1c2cbc4e4aad9ed8',
+    clientSecret: '6ca16373efa03347e11a96ff92e355c5cea189bb',
+    repo: 'Comment', // 评论仓库地址
+    owner: 'chanshiyucx',
+    admin: ['chanshiyucx'],
+    distractionFreeMode: false // 是否开始无干扰模式【背景遮罩】
+  },
 
   /**
    * leancloud 配置 【文章浏览次数】
@@ -26,18 +43,6 @@ export default {
   leancloud: {
     appId: 'b6DWxsCOWuhurfp4YqbD5cDE-gzGzoHsz',
     appKey: 'h564RR5uVuJV5uSeP7oFTBye'
-  },
-
-  /**
-   * Gittalk 配置【评论功能】，详细文档参见：https://github.com/gitalk/gitalk
-   */
-  gitalk: {
-    clientID: '864b1c2cbc4e4aad9ed8',
-    clientSecret: '6ca16373efa03347e11a96ff92e355c5cea189bb',
-    repo: 'Comment',
-    owner: 'chanshiyucx',
-    admin: ['chanshiyucx'],
-    distractionFreeMode: false // 是否开始无干扰模式【背景遮罩】
   },
 
   /**
@@ -50,14 +55,16 @@ export default {
    * 归档页面
    */
   archiveOpts: {
-    enableComment: false,
-    qoute: '文章千古事，得失寸心知'
+    display: true, // 是否显示该页面
+    enableComment: false, // 是否开启评论功能
+    qoute: '文章千古事，得失寸心知' // 页面顶部一言
   },
 
   /**
    * 分类页面【与 Github Issues 的 Milestone 对应】
    */
   categoryOpts: {
+    display: true,
     enableComment: false,
     qoute: '行云流水，落笔生花'
   },
@@ -66,6 +73,7 @@ export default {
    * 标签配置
    */
   tagOpts: {
+    display: true,
     enableComment: false,
     qoute: '列卒周匝，星罗云布'
   },
@@ -74,6 +82,7 @@ export default {
    * 心情页面
    */
   moodOpts: {
+    display: true,
     enableComment: true,
     qoute: '欲言又止，止言又欲'
   },
@@ -82,6 +91,7 @@ export default {
    * 书单页面
    */
   bookOpts: {
+    display: true,
     enableComment: true,
     qoute: '吾生也有涯，而知也无涯'
   },
@@ -90,6 +100,7 @@ export default {
    * 友链页面
    */
   friendOpts: {
+    display: true,
     enableComment: true,
     qoute: '青青子衿，悠悠我心'
   },
@@ -98,12 +109,13 @@ export default {
    * 关于页面
    */
   aboutOpts: {
+    display: true,
     enableComment: true,
     qoute: '蝉鸣如雨，花宵道中',
     avatar: 'https://i.loli.net/2018/12/09/5c0cc2b4e0195.png',
     graduated: 'University of Electronic Science and Technology of China (UESTC)',
     college: 'Communication&Information Engineering',
-    // 联系方式
+    // 联系方式，可自由添加
     contact: [
       {
         icon: 'https://i.loli.net/2019/01/25/5c4b2a7558ad7.png',
@@ -163,23 +175,6 @@ export default {
   },
 
   /**
-   * 主题配色，目前主要用于文章、说说、关于等卡片配色，以后可能会有其他用途
-   * 推荐一个好看的取色站，日本の伝統色：http://nipponcolors.com/
-   */
-  themeColors: [
-    '#B28FCE', // 薄
-    '#86C166', // 苗
-    '#F9BF45', // 玉子
-    '#FAD689', // 浅黄
-    '#F596AA', // 桃
-    '#E79460', // 洗柿
-    '#2EA9DF', // 露草
-    '#FFBA84', // 洒落柿
-    '#7DB9DE', // 勿忘草
-    '#FF99FF'
-  ],
-
-  /**
    * 音乐播放器,
    */
   APlayer: [
@@ -213,5 +208,22 @@ export default {
       url: 'https://files.catbox.moe/vwnawp.mp3',
       cover: 'https://i.loli.net/2018/12/09/5c0cc3d36349c.jpg'
     }
+  ],
+
+  /**
+   * 主题配色，目前主要用于文章、说说、关于等卡片配色，以后可能会有其他用途
+   * 推荐一个好看的取色站，日本の伝統色：http://nipponcolors.com/
+   */
+  themeColors: [
+    '#B28FCE', // 薄
+    '#86C166', // 苗
+    '#F9BF45', // 玉子
+    '#FAD689', // 浅黄
+    '#F596AA', // 桃
+    '#E79460', // 洗柿
+    '#2EA9DF', // 露草
+    '#FFBA84', // 洒落柿
+    '#7DB9DE', // 勿忘草
+    '#FF99FF'
   ]
 }
