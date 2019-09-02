a5fd659861e902303cd0544fc3832fbb81aad0a0
zhibi <1017866168@qq.com>
2019-09-02 15:12:18 +0800

    update 换成我自己的信息

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@ set -e
 # 构建
 npm run build
 
-# cd 到构建输出的目录下 
+# cd 到构建输出的目录下
 cd dist
 
 # 部署到自定义域名，没有自定义域名可注释掉
-echo 'chanshiyu.com' > CNAME
+echo 'xlzy520.cn' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:chanshiyucx/chanshiyucx.github.io.git master
+git push -f git@github.com:xlzy520/xlzy520.github.io.git master
 
 cd -
