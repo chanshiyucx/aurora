2a4a7e757fa5dbe0ab55b74ba9f209719304ef46
chanshiyu <me@chanshiyu.com>
2019-10-19 15:22:28 +0700

    feat: 添加meta标签

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   productionSourceMap: false,
   chainWebpack(config) {
     const cdn = {
-      css: ['//fonts.googleapis.com/css?family=Fira+Mono|Noto+Serif+SC'],
+      css: ['//fonts.googleapis.com/css?family=Fira+Mono|Noto+Serif+SC&display=swap'],
       js: [
         '//cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js',
         '//cdn.jsdelivr.net/npm/jquery-backstretch@2.1.17/jquery.backstretch.min.js'
