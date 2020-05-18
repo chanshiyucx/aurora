3d7fa6db19b61f32d067c4b58a15551eaaf49237
chanshiyu <me@chanshiyu.com>
2020-05-18 11:06:40 +0700

    update lib

--- src/assets/lib/highlight.js
+++ src/assets/lib/highlight.js
@@ -1,4 +1,4 @@
-import hljs from 'highlight.js/lib/highlight'
+import hljs from 'highlight.js/lib/core'
 import javascript from 'highlight.js/lib/languages/javascript'
 import xml from 'highlight.js/lib/languages/xml'
 import less from 'highlight.js/lib/languages/less'
