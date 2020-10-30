f564040c380f38a8554ea0fb362949c7d6e605a5
chanshiyu <chanshiyu@tg10010.com>
2020-10-30 17:40:17 +0400

    update: 文章详情跳转方式

--- src/components/ArchiveCard/index.vue
+++ src/components/ArchiveCard/index.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="archive">
     <ul class="content">
-      <li
-        class="cursor"
-        v-for="(post, i) in posts"
-        :key="post.id"
-        @click="gotoPost(post.number)"
-        :style="{ borderTopColor: colors[i] }"
-      >
-        <h3>{{ post.title }}</h3>
-        <div class="post-meta">
-          <span>
-            <i class="icon icon-calendar"></i>
-            {{ post.created_at }}
-          </span>
-          <span>
-            <i class="icon icon-fire"></i>
-            热度{{ times[post.id] || 1 }}℃
-          </span>
-          <span>
-            <i class="icon icon-bookmark-empty"></i>
-            {{ post.milestone ? post.milestone.title : '未分类' }}
-          </span>
-          <span>
-            <i class="icon icon-tag"></i>
-            <span v-for="label in post.labels.slice(0, 2)" :key="label.id">{{ label.name }}</span>
-          </span>
-        </div>
+      <li class="cursor" v-for="(post, i) in posts" :key="post.id" :style="{ borderTopColor: colors[i] }">
+        <router-link :to="{ name: 'post', params: { number: post.number, post } }">
+          <h3>{{ post.title }}</h3>
+          <div class="post-meta">
+            <span>
+              <i class="icon icon-calendar"></i>
+              {{ post.created_at }}
+            </span>
+            <span>
+              <i class="icon icon-fire"></i>
+              热度{{ times[post.id] || 1 }}℃
+            </span>
+            <span>
+              <i class="icon icon-bookmark-empty"></i>
+              {{ post.milestone ? post.milestone.title : '未分类' }}
+            </span>
+            <span>
+              <i class="icon icon-tag"></i>
+              <span v-for="label in post.labels.slice(0, 2)" :key="label.id">{{ label.name }}</span>
+            </span>
+          </div>
+        </router-link>
       </li>
     </ul>
     <div class="btn-group" v-if="!isDisabledPrev || !isDisabledNext">
@@ -80,10 +76,6 @@ export default {
     // 翻页
     handlePage(type) {
       this.$emit('handlePage', type)
-    },
-    // 跳转文章页
-    gotoPost(number) {
-      this.$router.push({ name: 'post', params: { number } })
     },
   },
 }
